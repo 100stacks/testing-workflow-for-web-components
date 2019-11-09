22dc46712a4bb4dd255df8b6ed7512fb82ce976e
100stacks <james.thompson@rackspace.com>
2019-11-09 16:49:19 -0600

    chore(step04): init test now passes

--- src/a11y-input.js
+++ src/a11y-input.js
@@ -5,6 +5,16 @@ import { LitElement, html, css } from 'lit-element';
 
 export class A11yInput extends LitElement {
   // Init class
-}
+  static get properties () {
+    return {
+      label: { type: String },
+    };
+  };
+
+  constructor () {
+    super();
+    this.label = '';
+  };
+};
 
 customElements.define('a11y-input', A11yInput);
