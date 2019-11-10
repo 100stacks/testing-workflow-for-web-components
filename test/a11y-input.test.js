c279a571fe9dcf3ca5f20479abc521a8d56b02d6
100stacks <james.thompson@rackspace.com>
2019-11-10 03:26:25 -0600

    test(light dom): add chai lightDom test

--- test/a11y-input.test.js
+++ test/a11y-input.test.js
@@ -28,10 +28,27 @@ describe('a11y input', () => {
   //   );
   // }); // innerHTML relies on simple string equality
   it('has a static shadowDOM', async () => {
-    const el = /** @type {A11yInput} */ (await fixture(html`<a11y-input></a11y-input>`));
+    const el = /** @type {A11yInput} */ (await fixture(
+      html`
+        <a11y-input></a11y-input>
+      `,
+    ));
     expect(el).shadowDom.to.equal(`
       <slot name="label"></slot>
       <slot name="input"></slot>
+    `);
+  });
+
+  /**
+   * Test Light DOM
+   */
+  it('has 1 input and 1 label in the Light DOM', async () => {
+    const el = /** @type {A11yInput} */ (await fixture(html`
+      <a11y-input .label=${'foo'}></a11y-input>
+    `));
+    expect(el).lightDom.to.equal(`
+      <label slot="label">foo</label>
+      <input slot="input">
     `);
   });
 });
