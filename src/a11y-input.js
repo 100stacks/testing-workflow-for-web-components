b44e4658d7616625245320cb26e55a32311615c7
100stacks <james.thompson@rackspace.com>
2019-11-10 03:34:46 -0600

    test(light dom): add Light DOM connectedCallback

--- src/a11y-input.js
+++ src/a11y-input.js
@@ -23,6 +23,21 @@ export class A11yInput extends LitElement {
       <slot name="input"></slot>
     `;
   };
+
+  /**
+   * Light DOM
+   */
+  connectedCallback () {
+    super.connectedCallback();
+    this.labelEl = document.createElement('label');
+    this.labelEl.innerText = this.label;
+    this.labelEl.setAttribute('slot', 'label');
+    this.appendChild(this.labelEl);
+
+    this.inputEl = document.createElement('input');
+    this.inputEl.setAttribute('slot', 'input');
+    this.appendChild(this.inputEl);
+  };
 };
 
 customElements.define('a11y-input', A11yInput);
