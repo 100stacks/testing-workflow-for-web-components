fe86aa257e22ab0d0521a3de7f8490005067fd05
100stacks <100stacks@users.noreply.github.com>
2020-03-11 17:18:25 -0500

    chore: add skeleton app and initial failing test

---
+++ a11y-input/src/my-app.js
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+// eslint-disable-next-line no-unused-vars
+import { LitElement, html, css } from "lit-element";
+
+export class MyApp extends LitElement {
+  // content placeholder
+}
+
+customElements.define("my-app", MyApp);
