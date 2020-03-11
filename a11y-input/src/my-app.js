3c4599372eeb92bb503d860d9dd38dc2b175dc35
100stacks <100stacks@users.noreply.github.com>
2020-03-11 17:36:43 -0500

    chore: add MyApp render method

--- a11y-input/src/my-app.js
+++ a11y-input/src/my-app.js
@@ -4,6 +4,13 @@ import { LitElement, html, css } from "lit-element";
 
 export class MyApp extends LitElement {
   // content placeholder
+
+  render() {
+    return html`
+      <h1>My Filter App</h1>
+      <a11y-input></a11y-input>
+    `;
+  }
 }
 
 customElements.define("my-app", MyApp);
